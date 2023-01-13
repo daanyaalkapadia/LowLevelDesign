2ff29689b3a2436ae5b7ea573eddb941dd56c8cc
Daanyaal Kapadia <daanyaalkapadia@outlook.com>
2023-01-13 07:36:30 +0530

    Add project files.

---
+++ LowLevelDesign/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LowLevelDesign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
