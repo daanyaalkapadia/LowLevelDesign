660d7dea6372cfd030defba2a28daf02bd5058ea
Daanyaal Kapadia <daanyaalkapadia@outlook.com>
2023-01-13 12:29:07 +0530

    Builder Design Pattern

---
+++ LowLevelDesign/Builder/IStudentBuilder.cs
@@ -0,0 +1,13 @@
+﻿using static LowLevelDesign.Builder.Student;
+
+namespace LowLevelDesign.Builder
+{
+    internal interface IStudentBuilder
+    {
+        StudentBuilder SetName(string name);
+        StudentBuilder SetId(int id);
+        StudentBuilder SetPercentage(double percentage);
+        StudentBuilder SetCGPA(double cgpa);
+        Student Build();
+    }
+}
---
+++ LowLevelDesign/Builder/StudentDirector.cs
@@ -0,0 +1,14 @@
+﻿namespace LowLevelDesign.Builder
+{
+    internal class StudentDirector
+    {
+        public Student GetSchoolStudentObj(int id, string name, double percentage)
+        {
+            return Student.GetBuilder().SetId(id).SetName(name).SetPercentage(percentage).Build();
+        }
+        public Student GetCollegeStudentObj(int id, string name, double cgpa)
+        {
+            return Student.GetBuilder().SetId(id).SetName(name).SetCGPA(cgpa).Build();
+        }
+    }
+}
